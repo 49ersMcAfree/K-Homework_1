4356dab37e65ef1572382bf0a6d9cbaf3acaba9a
CWill22 <147526756+CWill22@users.noreply.github.com>
2024-01-30 16:13:30 -0600

    Update Homework_1.java

--- Homework_1/src/edu/mu/Homework_1.java
+++ Homework_1/src/edu/mu/Homework_1.java
@@ -2,6 +2,7 @@ package edu.mu;
 
 public class Homework_1 {
 	public static void main(String[] args) {
+		//Print: Name, GIT ID
 		System.out.println("Will Curry, GIT ID: CWill22");
 		System.out.println("Ethan Darling, GIT ID: edar115");
 		System.out.println("Alasdair Orr, GIT ID: 49ersMcAfree");
