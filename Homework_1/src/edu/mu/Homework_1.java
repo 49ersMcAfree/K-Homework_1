a85fc039db651155133439de1576bed76974045c
edar115 <158128266+edar115@users.noreply.github.com>
2024-01-30 16:07:00 -0600

    Update Homework_1.java

--- Homework_1/src/edu/mu/Homework_1.java
+++ Homework_1/src/edu/mu/Homework_1.java
@@ -3,7 +3,7 @@ package edu.mu;
 public class Homework_1 {
 	public static void main(String[] args) {
 		System.out.println("Will Curry, GIT ID: CWill22");
-		System.out.println("Ethan Darling,GIT ID: edar115");
+		System.out.println("Ethan Darling, GIT ID: edar115");
 		System.out.println("Alasdair Orr, GIT ID: 49ersMcAfree");
 		System.out.println("Braedon Penrod, GIT ID: oRackzyy");
 	}
