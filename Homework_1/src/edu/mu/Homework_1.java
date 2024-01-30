2085068c8308f50624156538a30e64e98fa39818
Braeden <158224803+oRackzyy@users.noreply.github.com>
2024-01-30 16:08:15 -0600

    NameChange.java

--- Homework_1/src/edu/mu/Homework_1.java
+++ Homework_1/src/edu/mu/Homework_1.java
@@ -5,6 +5,6 @@ public class Homework_1 {
 		System.out.println("Will Curry, GIT ID: CWill22");
 		System.out.println("Ethan Darling, GIT ID: edar115");
 		System.out.println("Alasdair Orr, GIT ID: 49ersMcAfree");
-		System.out.println("Braedon Penrod, GIT ID: oRackzyy");
+		System.out.println("Braeden Penrod, GIT ID: oRackzyy");
 	}
 }
